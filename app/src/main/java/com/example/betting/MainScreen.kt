98e41ae96740ee55431a34aec41f5800ac842b3b
Igor Chaus <igor_chaus@mail.ru>
2023-10-01 07:28:56 +0200

    Made first free screens

---
+++ app/src/main/java/com/example/betting/MainScreen.kt
@@ -0,0 +1,31 @@
+package com.example.betting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.betting.databinding.MainScreenBinding
+
+
+class MainScreen : Fragment() {
+
+    private var _binding: MainScreenBinding? = null
+    private val binding: MainScreenBinding
+        get() = _binding ?: throw RuntimeException("MainScreenBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = MainScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bottomNav.setItemIconTintList(null)
+    }
+}
