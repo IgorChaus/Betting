8d622b203dbdbc9ebe77f7c51be934268baf7ca0
Igor Chaus <igor_chaus@mail.ru>
2023-10-07 21:22:38 +0200

    Added settings_screen

---
+++ app/src/main/java/com/example/betting/ListScreen.kt
@@ -0,0 +1,35 @@
+package com.example.betting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.betting.databinding.ListScreenBinding
+
+class ListScreen : Fragment() {
+
+    private var _binding: ListScreenBinding? = null
+    private val binding: ListScreenBinding
+        get() = _binding ?: throw RuntimeException("ListScreenBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = ListScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object{
+        fun getInstance() = ListScreen()
+    }
+
+}
