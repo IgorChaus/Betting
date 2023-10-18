5044f8cbc28e0a7221ef5c9f7fc02ba7947e158a
Igor Chaus <igor_chaus@mail.ru>
2023-10-18 20:44:23 +0200

    Moved launch SplashScreen from onResume to onCreate

--- app/src/main/java/com/example/betting/MainActivity.kt
+++ app/src/main/java/com/example/betting/MainActivity.kt
@@ -8,27 +8,24 @@ import com.example.betting.view.WelcomeScreen
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var prefs: SharedPreferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        prefs = getSharedPreferences("com.example.betting", MODE_PRIVATE)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (prefs.getBoolean(FIRST_LAUNCH, true)) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container_activity, SplashScreen.getInstance())
-                .commit()
-            prefs.edit().putBoolean(FIRST_LAUNCH, false).apply()
-        }else {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container_activity, WelcomeScreen.getInstance())
-                .commit()
+        if (savedInstanceState == null) {
+            val prefs = getSharedPreferences("com.example.betting", MODE_PRIVATE)
+            if (prefs.getBoolean(FIRST_LAUNCH, true)) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_activity, SplashScreen.getInstance())
+                    .commit()
+                prefs.edit().putBoolean(FIRST_LAUNCH, false).apply()
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_activity, WelcomeScreen.getInstance())
+                    .commit()
+            }
         }
     }
+
 
     companion object{
         private const val FIRST_LAUNCH = "first_launch"
