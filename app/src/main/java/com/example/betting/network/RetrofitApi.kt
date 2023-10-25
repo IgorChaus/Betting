042fed4a7cd9fe7116c8a80eb6f05693a8a0108f
Igor Chaus <igor_chaus@mail.ru>
2023-10-25 22:29:41 +0200

    Added RetrofitApi to GitHub

---
+++ app/src/main/java/com/example/betting/network/RetrofitApi.kt
@@ -0,0 +1,31 @@
+package com.example.betting.network
+
+import com.example.betting.model.LeaguesResponse
+import com.example.betting.model.PlayersResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface RetrofitApi {
+    @Headers(
+        "x-rapidapi-key: XXXXXXXXXXXXXXXXXXXXX",
+        "x-rapidapi-host: v3.football.api-sports.io"
+    )
+    @GET("leagues")
+    suspend fun getLeagues(
+        @Query("name") name: String,
+        @Query("season") season: String
+    ) : Response<LeaguesResponse>
+
+    @Headers(
+        "x-rapidapi-key: XXXXXXXXXXXXXXXXXXXXXX",
+        "x-rapidapi-host: v3.football.api-sports.io"
+    )
+    @GET("players")
+    suspend fun getPlayers(
+        @Query("league") league: String,
+        @Query("season") season: String,
+        @Query("page") page: String
+    ) : Response<PlayersResponse>
+}
